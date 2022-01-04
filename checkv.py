eefb79ad66fe95c7ceec33c7f028a4df1108b1c2
Nicolas <78997194+nicolaspiet@users.noreply.github.com>
2022-01-04 19:39:48 -0300

    YouTube Video Downloader - 1.5v
    Added a message box when the user has one or more outdated packages, asking if he wants to continue anyway.

---
+++ checkv.py
@@ -0,0 +1,66 @@
+from importlib.metadata import distribution
+from tkinter import messagebox
+ttkthemes_version = distribution("ttkthemes")
+pytube_version = distribution("pytube")
+outdated_packages = []
+
+
+def list_outdated_packages():
+    return "\n".join(outdated_packages)
+
+
+def check_ttkthemes():
+    # Separando a versão
+    ttkthemes_sp = ttkthemes_version.version.split(".")
+    peste2 = []
+    # For para transformar cada item da lista em um inteiro.
+    for item in ttkthemes_sp:
+        item1 = int(item)
+        peste2.append(item1)
+    if peste2[0] >= 3:
+        if peste2[1] >= 2:
+            if peste2[2] >= 2:
+                print(peste2)
+                return True
+            return False
+        return False
+    return False
+
+
+def check_pytube():
+    # Separando a versão
+    pytube_sp = pytube_version.version.split(".")
+    peste = []
+    # For para transformar cada item da lista em um inteiro.
+    for item in pytube_sp:
+        item1 = int(item)
+        peste.append(item1)
+    if peste[0] >= 12:
+        if peste[2] >= 2:
+            print(peste)
+            return True
+        return False
+    return False
+
+
+def check():
+    if check_ttkthemes():
+        pass
+    else:
+        ttkthemes_np = "ttkthemes " + ttkthemes_version.version
+        outdated_packages.append(ttkthemes_np)
+    if check_pytube():
+        pass
+    else:
+        pytube_np = "pytube " + pytube_version.version
+        outdated_packages.append(pytube_np)
+        text = f"You're using a outdated version of the following packages:\n\n{list_outdated_packages()}\n\nIf you continue, the program may not working properly.\nDo you want to continue anyway?"
+        res = messagebox.askquestion('Do you want to continue?', text)
+        if res == "yes":
+            pass
+        elif res == "no":
+            exit()
+
+
+if "__name__" == "__main__":
+    pass
--- main.py
+++ main.py
@@ -1,112 +1,124 @@
-import ttkthemes.themed_tk
-from pytube import YouTube
-import tkinter
-from tkinter import ttk
-from tkinter import filedialog
-import tkinter.font as tkFont
-import os
+try:
+    from checkv import *
 
-# Functions
+    import ttkthemes.themed_tk
+    from pytube import YouTube
+    import tkinter
+    from tkinter import ttk
+    from tkinter import filedialog
+    import tkinter.font as tkFont
+    import os
+    from importlib.metadata import distribution
 
-
-def download_text():
-    download_button["text"] = "Download"
-
-
-def info():
-    try:
-        global youtube_video
-        youtube_link = video_link.get()
-        youtube_video = YouTube(youtube_link)
-        select_quality()
-    except Exception as err:
-        download_button["text"] = "Some error ocurred, verify the link and try again."
-        print(err)
-        main_window.after(3000, download_text)
+    # Verify packages version
 
 
-def download():
-    global youtube_path
-    global downloading_label
-    resolution_r.destroy()
-    youtube_path = filedialog.askdirectory(title="Select a File")
-    if youtube_path == '':
-        pass
-    else:
-        download_button['text'] = "Downloading..."
-        if value_inside2.get() == "MP4":
-            main_window.after(30, download_mp4)
+
+    # Functions
+
+    def download_text():
+        download_button["text"] = "Download"
+
+
+    def info():
+        try:
+            global youtube_video
+            youtube_link = video_link.get()
+            youtube_video = YouTube(youtube_link)
+            select_quality()
+        except Exception as err:
+            download_button["text"] = "Some error ocurred, verify the link and try again."
+            print(err)
+            main_window.after(3000, download_text)
+
+
+    def download():
+        global youtube_path
+        global downloading_label
+        resolution_r.destroy()
+        youtube_path = filedialog.askdirectory(title="Select a File")
+        if youtube_path == '':
+            pass
         else:
-            main_window.after(30, download_mp3)
+            download_button['text'] = "Downloading..."
+            if value_inside2.get() == "MP4":
+                main_window.after(30, download_mp4)
+            else:
+                main_window.after(30, download_mp3)
 
 
-def download_mp3():
-    final_video = youtube_video.streams.filter(only_audio=True).first()
-    out_file = final_video.download(youtube_path)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    download_button['text'] = "Successfully downloaded!"
-    main_window.after(2500, download_text)
+    def download_mp3():
+        final_video = youtube_video.streams.filter(only_audio=True).first()
+        out_file = final_video.download(youtube_path)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        download_button['text'] = "Successfully downloaded!"
+        main_window.after(2500, download_text)
 
 
-def download_mp4():
-    youtube_video.streams.filter(res=value_inside.get()).first().download(youtube_path)
-    download_button['text'] = "Successfully downloaded!"
-    main_window.after(2500, download_text)
+    def download_mp4():
+        youtube_video.streams.filter(res=value_inside.get()).first().download(youtube_path)
+        download_button['text'] = "Successfully downloaded!"
+        main_window.after(2500, download_text)
 
 
-def select_quality():
-    global formats
-    global value_inside
-    global value_inside2
-    global resolution_r
+    def select_quality():
+        global formats
+        global value_inside
+        global value_inside2
+        global resolution_r
+        # Window
+        resolution_r = tkinter.Toplevel()
+        resolution_r.title("Download Settings")
+        resolution_r.geometry("270x130")
+        resolution_r.resizable(False, False)
+        resolution_r.iconbitmap("download.ico")
+        # Getting available resolutions for the video
+        formats = ["MP4", "MP4", "MP3"]
+        video_resolutions = [stream.resolution for stream in youtube_video.streams.filter(progressive=True)]
+        video_resolutions.insert(0, '')
+        value_inside = tkinter.StringVar(resolution_r)
+        value_inside.set(video_resolutions[-1])
+        value_inside2 = tkinter.StringVar(resolution_r)
+        value_inside2.set(formats[1])
+        question_menu = ttk.OptionMenu(resolution_r, value_inside, *video_resolutions)
+        question_menu.grid(column=0, row=0, padx=(65, 3), pady=15)
+        format_menu = ttk.OptionMenu(resolution_r, value_inside2, *formats)
+        format_menu.grid(column=1, row=0)
+        next_button = ttk.Button(resolution_r, text="Next", command=download)
+        next_button.place(x=88, y=65)
+
+
     # Window
-    resolution_r = tkinter.Toplevel()
-    resolution_r.title("Download Settings")
-    resolution_r.geometry("270x130")
-    resolution_r.resizable(False, False)
-    resolution_r.iconbitmap("download.ico")
-    # Getting available resolutions for the video
-    formats = ["MP4", "MP4", "MP3"]
-    video_resolutions = [stream.resolution for stream in youtube_video.streams.filter(progressive=True)]
-    video_resolutions.insert(0, '')
-    value_inside = tkinter.StringVar(resolution_r)
-    value_inside.set(video_resolutions[-1])
-    value_inside2 = tkinter.StringVar(resolution_r)
-    value_inside2.set(formats[1])
-    question_menu = ttk.OptionMenu(resolution_r, value_inside, *video_resolutions)
-    question_menu.grid(column=0, row=0, padx=(65, 3), pady=15)
-    format_menu = ttk.OptionMenu(resolution_r, value_inside2, *formats)
-    format_menu.grid(column=1, row=0)
-    next_button = ttk.Button(resolution_r, text="Next", command=download)
-    next_button.place(x=88, y=65)
 
+    main_window = ttkthemes.themed_tk.ThemedTk(theme='breeze', themebg=True, toplevel=True)
+    main_window.title("YouTube Video Downloader")
+    main_window.geometry("405x170")
+    main_window.resizable(False, False)
+    main_window.iconbitmap("download.ico")
+    font_style = tkFont.Font(family="Lucida Grande", size=12)
 
-# Window
+    # Widgets
 
-main_window = ttkthemes.themed_tk.ThemedTk(theme='breeze', themebg=True, toplevel=True)
-main_window.title("YouTube Video Downloader")
-main_window.geometry("405x170")
-main_window.resizable(False, False)
-main_window.iconbitmap("download.ico")
-font_style = tkFont.Font(family="Lucida Grande", size=12)
+    instruction_text = ttk.Label(main_window, text="Insert the YouTube video link below:", font=font_style)
+    video_link = ttk.Entry(main_window, width=43)
+    download_button = ttk.Button(main_window, text="Download", command=info)
 
-# Widgets
+    # Grids
 
-instruction_text = ttk.Label(main_window, text="Insert the YouTube video link below:", font=font_style)
-video_link = ttk.Entry(main_window, width=43)
-download_button = ttk.Button(main_window, text="Download", command=info)
+    instruction_text.grid(column=0, row=0, padx=12, pady=9)
+    video_link.grid(column=0, row=1, padx=47, pady=10)
+    download_button.grid(column=0, row=2, padx=10, pady=10)
+    check()
+    main_window.mainloop()
 
-# Grids
-
-instruction_text.grid(column=0, row=0, padx=12, pady=9)
-video_link.grid(column=0, row=1, padx=47, pady=10)
-download_button.grid(column=0, row=2, padx=10, pady=10)
-
-main_window.mainloop()
-
-"""
-
+    """
     
-"""
+        
+    """
+
+except ModuleNotFoundError:
+    print("Unable to find one or more of the required packages, please install or update them.\nPackages:\n\n- pytube\n"
+          "- ttkthemes\n")
+    input("Press ENTER to exit the application.")
